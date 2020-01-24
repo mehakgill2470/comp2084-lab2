e3f0fbd5ada716d3d9b6ec77d570320774dcb05e
Mehak Gill <mehakgill2470@gmail.com>
2020-01-23 20:27:52 -0500

    completed lab2

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            string[] products = { "laptop", "desktop", "tablet" };
+            ViewData["products"] = products;
+            return View();
+        }
+
+        public string Details(string product)
+        {
+            string message = "Selection = " + product;
+            return message;
+        }
+    }
+}
